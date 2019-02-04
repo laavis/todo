ceada61b6355afcb588135f7cc13a8cae60b19d2
laavis <sara.suviranta@gmail.com>
2019-02-04 16:41:07 +0200

    Updates

---
+++ precache-manifest.37073a10d8ba3059021ff1cdb5e41806.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "0fa400ff73ed2d76a277",
+    "url": "/todo/static/js/main.0fa400ff.chunk.js"
+  },
+  {
+    "revision": "8e9a056dad337434741e",
+    "url": "/todo/static/js/1.8e9a056d.chunk.js"
+  },
+  {
+    "revision": "764eaf006eb28e10997f5ddbcab8bcc6",
+    "url": "/todo/index.html"
+  }
+];
